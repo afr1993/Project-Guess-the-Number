fa13533765e42250687d47c45c58fb86b811e036
afr1993 <afr.1993@gmail.com>
2025-04-28 00:39:20 -0700

    First commit test

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean play = true;
+
+        System.out.println("Guest The number:");
+
+
+    }
+    }
