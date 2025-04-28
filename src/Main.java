24b43994453dca518d073b4ff0caf1f0fb2b910c
afr1993 <afr.1993@gmail.com>
2025-04-28 01:45:53 -0700

    Version 1 no optionals

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,66 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         boolean play = true;
+        boolean winner = false;
+        int min = 1;
+        int max = 100;
+        int ramndomNumber = random.nextInt(max - min + 1) + min;
+        int maxAttempts = 10;
+        int attempts = 0;
 
-        System.out.println("Guest The number:");
+        while(play){
+            System.out.println("Guest the number between "+min+" and "+max);
 
+            while(attempts < maxAttempts){
+                System.out.println("Enter your guest: ");
+
+                //Verification of input int
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Invalid input. \nEnter a number:");
+                    scanner.next();
+                    continue;
+                }
+
+                int userNumber = scanner.nextInt();
+
+                if (userNumber < min || userNumber > max) {
+                    System.out.println("Your guess is out of bounds ");
+                    continue;
+                }
+                attempts++;
+
+
+                if (ramndomNumber == userNumber) { 
+                    winner = true;
+                    break;
+                } else if (userNumber > ramndomNumber) {
+                    System.out.println("Number too High try again");
+                }else{
+                    System.out.println("Number to Low try again");
+                }
+
+                System.out.println("You still have: "+(maxAttempts-attempts)+" attempts");
+
+            }
+
+            if(winner){
+                System.out.println("You Win in the attempt: "+attempts);
+            }else {
+                System.out.println("Sorry you loose");
+            }
+
+            attempts = 0;
+
+            System.out.println("Play again (yes/no)");
+            String response = scanner.next();
+
+            if(response.equals("no")) {
+                play = false;
+            }
+        }
+
+        System.out.println("Thanks for playing! Goodbye.");
+        scanner.close();
 
     }
-    }
+}
